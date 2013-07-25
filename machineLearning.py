b822dfeda8f7c71f9ef32b8b164a3387f1aa3261
Harrison Sweeney <h4rrison.james@gmail.com>
2013-07-25 09:42:14 +0800

    added cross-validation

--- machineLearning.py
+++ machineLearning.py
@@ -4,7 +4,7 @@
 
 import MySQLdb as mdb, sys, os # MySQL Support Libraries
 import numpy as np
-from sklearn import svm, metrics
+from sklearn import metrics, cross_validation
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import BernoulliNB, MultinomialNB
@@ -50,7 +50,7 @@ if __name__ == '__main__':
 	# Retreive the data from the MySQL database
 	result = import_data()
 	corpus = result[0]
-	target = result[1]
+	target = np.array(result[1]) # Has to be of type np.array for cross-validation
 	indegree = result[2]
 	outdegree = result[3]
 	
@@ -61,38 +61,25 @@ if __name__ == '__main__':
 	n_samples = len(target)
 	data = X.toarray()
 	
-	print 'numrows: %d' % len(data)
-	print 'numcolumns: %d' % len(data[0])
-	
 	# Add the in-degree and out-degree to the data
 	data = np.column_stack((data, indegree, outdegree))
 	
 	# Normalize the data using the tf-idf transform
 	transformer = TfidfTransformer()
 	tfidf = transformer.fit_transform(data)
+	data = tfidf.toarray()
 	
 	# Create the classifiers
 	classifiers = [
 		DecisionTreeClassifier(random_state=0), # Decision Tree
 		MultinomialNB(alpha=.01), # Naive Bayes (Multinomial)
-		#BernoulliNB(alpha=.01), # Naive Bayes (Bernoulli)
-		#RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1), # Random Forest
-		#KNeighborsClassifier(3), # K-Neighbours
-		#LDA(), # Linear Discriminant Analysis
-		#QDA() # Quadratic Discriminant Analysis
+		RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1), # Random Forest
+		KNeighborsClassifier(3), # K-Neighbours
 	]
 	
 	for csf in classifiers:
-		# Train the classifier
-		n_train = 800
-		csf.fit(data[:n_train], target[:n_train])
-
-		# Now predict the outcome using the classifier
-		expected = target[n_train:]
-		predicted = csf.predict(data[n_train:])
-
-		print "="*80
-		print "Classification report for classifier %s:\n\n%s" % (
-		    csf, metrics.classification_report(expected, predicted))
-		#print "Confusion matrix:\n%s\n" % metrics.confusion_matrix(expected, predicted)
+		# Train the classifier using cross-validation
+		scores = cross_validation.cross_val_score(csf, data, target, cv=5)
+		print "Classification scores  for classifier %s:" % str(csf).split('(')[0]
+		print "Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() / 2)
 		

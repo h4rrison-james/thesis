5dee2677eb24aca9d9c4ceb846ed14418339fbb4
Harrison Sweeney <h4rrison.james@gmail.com>
2013-08-15 00:27:06 +0800

    added univariate feature selection

--- machineLearning.py
+++ machineLearning.py
@@ -4,15 +4,18 @@
 
 import MySQLdb as mdb, sys, os # MySQL Support Libraries
 import numpy as np
-from sklearn import metrics, cross_validation
+import pylab as pl
+
+from sklearn import metrics, cross_validation, datasets, svm
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB, GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.lda import LDA
 from sklearn.qda import QDA
+from sklearn.feature_selection import SelectPercentile, f_classif, SelectKBest, chi2
 
 def import_data():
 	# Connect to the MySQL Database
@@ -45,6 +48,29 @@ def import_data():
 		
 		return (descriptions, targets, indegrees, outdegrees)
 
+def normalise_data(data):
+	# Normalize the data using the tf-idf transform
+	transformer = TfidfTransformer()
+	tfidf = transformer.fit_transform(data)
+	data = tfidf.toarray()
+	return data
+
+def run_classifiers(data, target):
+	# Create the classifiers
+	classifiers = [
+		#DecisionTreeClassifier(random_state=0), # Decision Tree
+		MultinomialNB(alpha=0.01), # Naive Bayes (Multinomial)
+		#RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1), # Random Forest
+		#KNeighborsClassifier(3), # K-Neighbours
+	]
+	
+	for csf in classifiers:
+		# Train the classifier using cross-validation
+		# The 'scoring' parameter can be changed to a number of options
+		scores = cross_validation.cross_val_score(csf, data, target, cv=10, scoring='f1')
+		print "Classification scores  for %s:" % str(csf).split('(')[0]
+		print "F1: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() / 2)
+
 #### Main execution function ####
 if __name__ == '__main__':
 	# Retreive the data from the MySQL database
@@ -57,29 +83,28 @@ if __name__ == '__main__':
 	# Count using 'bag of words' and format data
 	vectorizer = CountVectorizer(min_df=1)
 	X = vectorizer.fit_transform(corpus)
+	print '\n' + '='*8 + ' Raw Data ' + '='*8
 	print "n_samples: %d, n_features: %d" % X.shape
 	n_samples = len(target)
 	data = X.toarray()
+	run_classifiers(data, target)
+	
+	# Univariate feature selection
+	selector = SelectKBest(chi2, k=3000) #3000 gives highest F1
+	data = selector.fit_transform(data, target)
+	print '\n' + '='*8 + ' After Feature Selection ' + '='*8
+	print "n_samples: %d, n_features: %d" % data.shape
+	run_classifiers(data, target)
 	
 	# Add the in-degree and out-degree to the data
 	data = np.column_stack((data, indegree, outdegree))
+	print '\n' + '='*8 + ' Including Graph Data ' + '='*8
+	print "n_samples: %d, n_features: %d" % data.shape
+	run_classifiers(data, target)
 	
-	# Normalize the data using the tf-idf transform
-	transformer = TfidfTransformer()
-	tfidf = transformer.fit_transform(data)
-	data = tfidf.toarray()
-	
-	# Create the classifiers
-	classifiers = [
-		DecisionTreeClassifier(random_state=0), # Decision Tree
-		MultinomialNB(alpha=.01), # Naive Bayes (Multinomial)
-		RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1), # Random Forest
-		KNeighborsClassifier(3), # K-Neighbours
-	]
-	
-	for csf in classifiers:
-		# Train the classifier using cross-validation
-		scores = cross_validation.cross_val_score(csf, data, target, cv=5)
-		print "Classification scores  for classifier %s:" % str(csf).split('(')[0]
-		print "Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() / 2)
+	# Normalise the data
+	data = normalise_data(data)
+	print '\n' + '='*8 + ' After Normalizing Data ' + '='*8
+	print "n_samples: %d, n_features: %d" % data.shape
+	run_classifiers(data, target)
 		

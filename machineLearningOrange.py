0c394f120ff92d7a481859836642dddf5eec3b37
Harrison Sweeney <h4rrison.james@gmail.com>
2013-08-21 16:09:55 +0800

    changes to ensemble methods

--- machineLearningOrange.py
+++ machineLearningOrange.py
@@ -10,15 +10,23 @@ from array import array
 if __name__ == '__main__':
 
 	# Build table
-	data = Orange.data.Table('description_tab')
+	description_data = Orange.data.Table('description_tab')
+	degree_data = Orange.data.Table('degree_tab')
+	am_data = Orange.data.Table('am_tab')
+	
+	data = Orange.data.Table([description_data, degree_data, am_data])
 	
 	bayes = Orange.classification.bayes.NaiveLearner(name="bayes")
+	bayes_csf = bayes(description_data)
 	tree = Orange.classification.tree.SimpleTreeLearner(name="tree")
-	knn = Orange.classification.knn.kNNLearner(name="knn")
+	tree_csf = tree(am_data)
+	svm = Orange.classification.svm.SVMLearner(name="svm")
+	svm_csf = svm(degree_data)
+	
 
-	base_learners = [bayes, tree, knn]
+	base_learners = [bayes, tree, svm]
 	stack = Orange.ensemble.stacking.StackedClassificationLearner(base_learners)
-
-	learners = [stack, bayes, tree, knn]
-	res = Orange.evaluation.testing.cross_validation(learners, data, 10)
+	learners = [stack, bayes, tree, svm]
+	res = Orange.evaluation.testing.cross_validation(learners, data, 5)
 	print "\n".join(["%8s: %5.3f" % (l.name, r) for r, l in zip(Orange.evaluation.scoring.AUC(res), learners)])
+	
